086681e769de1f1817d6627e8bde7fdab4cb8717
chasewoodard93 <110763268+chasewoodard93@users.noreply.github.com>
2025-11-19 15:30:32 -0600

    Update index.ts

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -9,3 +9,4 @@ export { default as PLReport } from './PLReport';
 export { default as BudgetManager } from './BudgetManager';
 export { default as Login } from './Login';
 export { default as UserSettings } from './UserSettings';
+export { default as Logout } from './Logout';
